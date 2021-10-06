d3d8b225c57a7fda1925d3c0cb550b889ac1a181
Kaare <kaaremadsen1@gmail.com>
2021-10-06 11:54:10 +0200

    remove unused config elements

--- Models/RCloneConfig.cs
+++ Models/RCloneConfig.cs
@@ -2,12 +2,6 @@
 {
     public class RCloneConfig
     {
-        [YamlDotNet.Serialization.YamlMember(Alias = "rclone_config")]
-        public string ConfigAbsolutePath { get; set; }
-        [YamlDotNet.Serialization.YamlMember(Alias = "rc_user")]
-        public string User { get; set; }
-        [YamlDotNet.Serialization.YamlMember(Alias = "rc_pass")]
-        public string Pass { get; set; }
         [YamlDotNet.Serialization.YamlMember(Alias = "sleeptime")]
         public int SleepTime { get; set; }
     }
--- Models/SARotateConfig.cs
+++ Models/SARotateConfig.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Serilog;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
